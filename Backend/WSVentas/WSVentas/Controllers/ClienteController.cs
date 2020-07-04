1db5241d5b1d09383dd81cb29de120d07740a651
Juan Andres Frigo <juan.a.frigo@gmail.com>
2020-07-04 00:23:36 -0300

    Agrego arquitectura muy simpificado, comienzo de Ws para consultas a la DB

---
+++ Backend/WSVentas/WSVentas/Controllers/ClienteController.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using WSVentas.Models;
+using WSVentas.Models.Response;
+
+namespace WSVentas.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ClienteController : ControllerBase
+    {
+        [HttpGet]
+        public IActionResult Get()
+        {
+            Respuesta oRespuesta = new Respuesta();
+            oRespuesta.Exito = 0;
+            try
+            {
+
+                // una vez que termina lo del using todo lo de adentro se elimina, en este caso la conexion a la db que es el contexto
+                using (VentaHDLContext db = new VentaHDLContext())
+                {
+                    // Dentro del using se hacen las consultas, en este caso con Entity Framework
+                    var lst = db.Cliente.ToList();
+
+                    oRespuesta.Exito = 1;
+
+                    oRespuesta.Data = lst;
+
+                }
+            }
+            catch (Exception ex)
+            {
+                oRespuesta.Mensaje = ex.Message;
+
+            }
+            return Ok(oRespuesta);
+
+        }
+
+
+    }
+
+
+
+
+}
+
+
