7e268671662f9991d0e03014e96348b8aa34e9f7
Juan Andres Frigo <juan.a.frigo@gmail.com>
2020-09-19 21:01:03 -0300

    coneccion angular con WS, listado de clientes con tabla de material

--- Frontend/app/src/app/app.module.ts
+++ Frontend/app/src/app/app.module.ts
@@ -4,11 +4,13 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { MatSidenavModule, MatSidenav } from '@angular/material/sidenav';
 import { HomeComponent } from './home/home.component';
 import { ClienteComponent } from './cliente/cliente.component';
-
 import { HttpClientModule } from '@angular/common/http';
+
+//Angular material
+import { MatSidenavModule, MatSidenav } from '@angular/material/sidenav';
+import {MatTableModule} from '@angular/material/table';
 
 @NgModule({
   declarations: [
@@ -21,7 +23,7 @@ import { HttpClientModule } from '@angular/common/http';
     AppRoutingModule,
     BrowserAnimationsModule,
     MatSidenavModule,
-
+    MatTableModule,
     HttpClientModule,
   ],
   providers: [],
